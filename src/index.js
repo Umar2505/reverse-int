85467d3a3d1ba54139075ef273e50ef25b1a5a64
Umar <rustamovumar0@gmail.com>
2022-02-03 12:49:03 +0500

    fixed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var a, result = 0
+    if(n>0) {
+        while(n){
+            a = n % 10 
+            result = (result * 10) + a
+            n = n/10|0
+        }
+    }else {
+        var num=n*-1
+        while(num) {
+            a = num % 10 
+            result = (result * 10) + a
+            num = num/10|0
+        }
+    }
+    return result
 }
